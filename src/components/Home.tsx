e6f910d9ae33ec544e88c2b08658d982e0948d25
Luis Colon <aileen@Luis.local>
2024-07-22 12:55:22 -0400

    finish Home Page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./home.css";
 import "./button.css";
+import profileImage from "./profile-image.svg";
+
 interface HomeProps {
   title: string;
 }
@@ -8,17 +10,32 @@ interface HomeProps {
 function Home({ title }: HomeProps) {
   return (
     <div id="home">
-      <h1>Software Engineer</h1>
-      <h1>Web Developer</h1>
-      <h1>UI UX Designer</h1>
-      <p>
+      <div className="profile-section">
+        <img src={profileImage} alt="Profile" className="profile-img" />
+        <p className="greeting">
+          Hello! I Am <span className="name-highlight">Luis Colon</span>
+        </p>
+      </div>
+      <h1>
+        Software <span>Engineer</span>
+      </h1>
+      <h1>
+        Web <span>Developer</span>
+      </h1>
+      <h1>
+        UI UX <span>Designer</span>
+      </h1>
+      <p className="description">
         Dynamic Software Engineer with 3+ years in web development, specializing
         in React, React Native, and JavaScript. Proven in enhancing UIs and
         modernizing codebases. Certified in Full Stack Engineering and UI/UX
         design. Bilingual in English and Spanish. Ready for a new challenge!
       </p>
-      <button>Get in Touch</button>
-      <button>Download CV</button>
+      <div className="buttons">
+        <button className="btn-primary">Get in Touch</button>
+        <button className="btn-secondary">CV Preview</button>
+      </div>
+      <div className="tech-icons"></div>
     </div>
   );
 }
